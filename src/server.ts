8ab9b3c39aaeec6195eaaa10e9df891b19baf762
artur <artur@unao.pl>
2018-06-16 13:48:07 +0200

    import types/external from go-network-framework to reuase improved types declarations

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+// we fixed and improved typings of some external packages here
+import 'go-network-framework/src/types/external'
+
 import * as util from 'ethereumjs-util'
 import * as gonetwork from 'go-network-framework'
 
-const world = 'WORLD';
+const world = 'WORLD!';
 
 export function hello(word: string = world): string {
   return `Hello ${world}! `;
