1532781bd12bdefba4e1e790adb19700610851e7
artur <artur@unao.pl>
2018-06-16 14:22:45 +0200

    make sure framework works - fix rxjs broken types by skipLibCheck - add node-fetch - add few todos

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 // we fixed and improved typings of some external packages here
-import 'go-network-framework/src/types/external'
+import 'go-network-framework/lib/types/external'
+
+// TODO: framework assumes fetch is there
+// we probably should handle server support a bit more graceful
+(global as any).fetch = require('node-fetch')
 
 import * as util from 'ethereumjs-util'
 import * as gonetwork from 'go-network-framework'
@@ -9,3 +13,8 @@ const world = 'WORLD!'
 export function hello (word: string = world): string {
   return `Hello ${world}! `
 }
+
+// TODO: remove it - it is here just to show that all should work fine
+import './monitoring-test'
+
+// TODO: choose yarn vs npm and stick to one across projects
