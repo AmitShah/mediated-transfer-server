c1698eca899dfe2de6b1fb5761bcc6411ef74ec2
artur <artur@unao.pl>
2018-06-16 13:51:59 +0200

    add tslint and fix src

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import 'go-network-framework/src/types/external'
 import * as util from 'ethereumjs-util'
 import * as gonetwork from 'go-network-framework'
 
-const world = 'WORLD!';
+const world = 'WORLD!'
 
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
+export function hello (word: string = world): string {
+  return `Hello ${world}! `
 }
